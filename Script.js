d6e867766d815b54407f88e475db5b9384f6f297
khatriaadarsh <aadarshkumark2003@gmail.com>
2025-01-28 00:55:26 +0500

    home section UI Completed

---
+++ Script.js
@@ -0,0 +1,5 @@
+const menuToggle = document.getElementById("menu-toggle");
+const menu = document.getElementById("menu");
+menuToggle.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+});

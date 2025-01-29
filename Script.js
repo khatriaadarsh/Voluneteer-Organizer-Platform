102a4d4ce242035d5f74dd30b99d42bd3fcafd74
khatriaadarsh <aadarshkumark2003@gmail.com>
2025-01-29 22:46:48 +0500

    ContactUs Section Completed

--- Script.js
+++ Script.js
@@ -3,3 +3,11 @@ const menu = document.getElementById("menu");
 menuToggle.addEventListener("click", () => {
   menu.classList.toggle("hidden");
 });
+
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    document.getElementById("confirmation").classList.remove("hidden");
+    this.reset();
+  });
